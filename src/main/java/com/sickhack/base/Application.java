a368a6902ddaa552a5fea4b1ed06725f75fd1122
Haruki Sato <haruki-sugarsun@users.noreply.github.com>
2017-02-02 03:42:47 +0900

    Initial Implementation.

---
+++ src/main/java/com/sickhack/base/Application.java
@@ -0,0 +1,13 @@
+package com.sickhack.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+@EnableAutoConfiguration
+@Import(AppConfiguration.class)
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/sickhack/base/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.sickhack.base.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @RequestMapping("/")
+    String home() {
+        return "index";
+    }
+}
