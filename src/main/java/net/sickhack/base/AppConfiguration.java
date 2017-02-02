e8e6fda82baab71410f5e312548c125eea32f72a
Haruki Sato <haruki-sugarsun@users.noreply.github.com>
2017-02-03 03:21:04 +0900

    Ignite sample.

--- src/main/java/net/sickhack/base/AppConfiguration.java
+++ src/main/java/net/sickhack/base/AppConfiguration.java
@@ -1,10 +1,16 @@
 package net.sickhack.base;
 
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteServices;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.IgniteConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import com.sickhack.base.ignite.MyCounterServiceImpl;
 
 /**
  * Core configuration for the base-server.
@@ -16,12 +22,30 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfiguration {
     private static final Logger logger = LoggerFactory.getLogger(AppConfiguration.class);
 
-    /**
-     * Fake bean creation to do initializations.
-     */
-    @Bean
-    Void initializations() {
-        logger.info("Initializing.");
-        return null;
-    }
+	/**
+	 * Fake bean creation to do initializations.
+	 */
+	@Bean
+	Void initializations(Ignite ignite) {
+		logger.info("Initializing.");
+		
+		// Ignite.
+		IgniteServices svcs = ignite.services();
+		svcs.deployClusterSingleton("myClusterSingleton", new MyCounterServiceImpl());
+		logger.info("services {}", svcs);
+		
+		return null;
+	}
+
+	@Bean
+	Ignite ignite(IgniteConfiguration igniteConfiguration) {
+		Ignite ignite = Ignition.start(igniteConfiguration);
+		return ignite;
+	}
+
+	@Bean
+	IgniteConfiguration igniteConfiguration() {
+		IgniteConfiguration config = new IgniteConfiguration();
+		return config;
+	}
 }
